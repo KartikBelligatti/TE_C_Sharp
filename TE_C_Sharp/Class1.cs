c4490959a618955bab129c2bb5c3fbdb5cdb753d
KartikBelligatti <belligattikartik17@gmail.com>
2022-09-23 11:22:07 +0530

    Add project files.

---
+++ TE_C_Sharp/Class1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TE_C_Sharp
+{
+    internal class Class1
+    {
+
+        public static void Main()
+        {
+            Console.WriteLine("Control state");
+            Conditional();
+            Class1 c = new Class1();
+            c.Looping();
+
+        }
+
+        public static void Conditional()
+        {
+            Console.WriteLine("Conditional");
+            Console.WriteLine("--------------------");
+            int a, b, c;
+            Console.WriteLine("Enter values");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if(a>b && a > c)
+            {
+                Console.WriteLine("A greater");
+            }
+            else if(b>a && b > c)
+            {
+                Console.WriteLine("B greater");
+            }
+            else
+            {
+                Console.WriteLine("C is greater");
+            }
+            Console.WriteLine("--------------------");
+
+        }
+
+        public  void Looping()
+        {
+            Console.WriteLine("looping");
+            int[] numbers = new int[] {1,2,3,4,5 };
+            int[] num = new int[7] { 1, 2, 3, 4, 5, 6, 7 };
+            int[] n = { 1, 2, 3 };
+
+            //for loop
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            Console.WriteLine("--------------------");
+
+
+            foreach (int i in num)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("--------------------");
+            int s = 0;
+            for(int i=0;i<n.Length; i++)
+            {
+                s += n[i];
+            }
+            Console.WriteLine("Sum is: {0}",s);
+            Console.WriteLine("--------------------");
+            int si;
+            Console.WriteLine("Enter size");
+            si = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[si]; 
+            for(int j = 0; j < si; j++)
+            {
+                Console.WriteLine("Enter the value of {0} of array", j + 1);
+                a[j] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            Console.WriteLine("--------------------");
+            Console.WriteLine("array elements");
+
+            foreach (int c in a)
+            {
+                Console.WriteLine(c);
+            }
+            
+
+
+        }
+
+    }
+}
